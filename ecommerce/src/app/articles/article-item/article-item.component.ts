41a72e5fd7d14f42d50b38a15bcd3756c9d6af12
Alex Fonte <aramosdf@gmail.com>
2024-12-02 22:13:25 +0100

    Feat: Ex3 canviar logica de añadir o quitar del carro + change detection

--- ecommerce/src/app/articles/article-item/article-item.component.ts
+++ ecommerce/src/app/articles/article-item/article-item.component.ts
@@ -1,11 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Article} from "../../model/article";
 import {ArticleQuantityChange} from "../../model/article-quantity-change";
 
 @Component({
   selector: 'app-article-item',
   templateUrl: './article-item.component.html',
-  styleUrl: './article-item.component.css'
+  styleUrl: './article-item.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class ArticleItemComponent implements OnInit {
 
